f7d019a88822838d605a4ee9de4eabfd2e75042c
CooperFu <cooperfu@CooperFuMacBook-Pro.local>
2025-11-26 10:07:17 +0800

    Configure custom domain vqlmusictime.com

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Music-time-machine-github/',
+  base: '/',
   plugins: [react()],
   test: {
     globals: true,
